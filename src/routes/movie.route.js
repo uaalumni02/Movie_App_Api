126168d31825c499146e5859a5a27157463dca86
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-11-26 21:57:23 -0500

    update movie route

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -6,16 +6,17 @@ import movieController from '../controllers/movie';
 const router = express.Router();
 
 
-router.route('/')
-  .post(checkAuth, movieController.addMovieData)
-
-  router.route('/:userId')
-  .get(checkAuth, movieController.getAllMovies)
 
   router.route('/:id')
   .delete(checkAuth, movieController.deleteMovie)
   .patch(checkAuth, movieController.updateMovie)
   .get(checkAuth, movieController.getMovieById)
+
+  router.route('/')
+  .post(checkAuth, movieController.addMovieData)
+
+  router.route('/:userId')
+  .get(checkAuth, movieController.getAllMovies)
 
 
 
